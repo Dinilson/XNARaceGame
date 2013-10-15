86648efa7cbf87b6b1b324a972b6732ecbd99964
Rubykuby <rubykuby@gmail.com>
2013-10-15 23:17:15 +0200

    Fix'd Checkpoint.cs a little.

--- XNARaceGame/Checkpoint.cs
+++ XNARaceGame/Checkpoint.cs
@@ -1,17 +1,25 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace XNARaceGame
 {
-	private class Checkpoint : Entity
+	class Checkpoint : Entity
 	{
         int num;
 
-		public Checkpoint(int num, Vector2 coords, Vector2 hitbox, rot) : base("Checkpoint", coords, hitbox, false, true, rot)
+		public Checkpoint(int num, Vector2 coords, Vector2 hitbox, double rot) : base("Checkpoint", coords, hitbox, false, true, rot)
 		{
             this.num = num;
-            //base.coords = coords;
-            //base.hitbox = hitbox;
-            //base.rot = rot;
+			// Because "Vector2 coords" is a parameter in "public Checkpoint()", it is forwarded to "coords" in base "base()".
+			// The same applies to "hitbox" and "rot".
 		}
 
         
