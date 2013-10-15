7fd8d13a63f730a942f8fdeabaa017459ac99d1a
gezichtshaar <tedteddet@gmail.com>
2013-10-15 22:52:49 +0200

    getters en settters gefixt + attribute initialisatie gefixt

--- XNARaceGame/Car.cs
+++ XNARaceGame/Car.cs
@@ -15,16 +15,17 @@ namespace XNARaceGame
 	{
 		#region Variables
 		//image = imagefile (needs work later)
-		double damage = 0; // Percentage. Preferably up to 100.
-		double petrol = 100; // Percentage. Starts with full petrol.
-		bool[] checkpoints = new bool[4]{ false, false, false, false }; // Four checkpoins. Initialises as having reached none (all false).
+		public double damage { get; set { if(value>=0 && <= 100) { damage = value} } // Percentage. Preferably up to 100.
+		public double petrol { get; set { if(value>=0 && <= 100) { petrol = value} } // Percentage. Starts with full petrol.
+		public bool[] checkpoints { get; set; } // Four checkpoins. Initialises as having reached none (all false).
 		#endregion
 
 		#region Constructor
 		public Car(Vector2 coords, double rot) : base("Car", coords, new Vector2(1337, 1337), true, true, rot) // Yet to define hitbox. Dummy values 1337.
 		{
-			base.coords = coords;
-			base.Rot = rot;
+			damage = 0;
+			petrol = 100;
+			checkpoints = { false, false, false, false };
 		}
 		#endregion
 	}
