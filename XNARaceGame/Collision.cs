afd436152c48094c255c3830f687f6a0319d1ca5
Rubykuby <rubykuby@gmail.com>
2013-10-15 17:35:30 +0200

    Formatted Lukas' code

--- XNARaceGame/Collision.cs
+++ XNARaceGame/Collision.cs
@@ -5,25 +5,32 @@ namespace XNARaceGame
 {
 	public class Collision
 	{
-		public static void CheckMapCollisions(int dt, Map map, List<Entity> entities) {
+		public static void CheckMapCollisions(int dt, Map map, List<Entity> entities)
+		{
 
-        }
+		}
 
-        public static void CheckEntityCollisions(int dt, List<Entity> entities) {
-            foreach (Entity entity in entities) {
-                if (entity.Actor) {
-                    foreach (Entity e in entities) {
-                        if (collides(entity, e)) {
-                            EntitiesCollide(entity, e);
-                        }
-                    }
-                }
-            }
-        }
+		public static void CheckEntityCollisions(int dt, List<Entity> entities)
+		{
+			foreach (Entity entity in entities)
+			{
+				if (entity.Actor)
+				{
+					foreach (Entity e in entities)
+					{
+						if (collides(entity, e))
+						{
+							EntitiesCollide(entity, e);
+						}
+					}
+				}
+			}
+		}
 
-        public static void EntitiesCollide(Entity collider, Entity colliding) {
+		public static void EntitiesCollide(Entity collider, Entity colliding)
+		{
 
-        }
+		}
 	}
 }
 
--- XNARaceGame/RaceGame.cs
+++ XNARaceGame/RaceGame.cs
@@ -11,98 +11,119 @@ using Microsoft.Xna.Framework.Media;
 
 namespace XNARaceGame
 {
-    class RaceGame : Microsoft.Xna.Framework.Game
-    {
-        private static int FPS = 60;
+	class RaceGame : Microsoft.Xna.Framework.Game
+	{
+		private static int FPS = 60;
 
-        private Map map;
-        private UI ui;
-        private Controller controller;
-        private View view;
-        private List<Entity> entities;
-        private bool running;
-        private int nextTick;
+		private Map map;
+		private UI ui;
+		private Controller controller;
+		private View view;
+		private List<Entity> entities;
+		private bool running;
+		private int nextTick;
 
-        public RaceGame()
-        {
-            running = false;
-            map=new Map();
-            ui=new UI(this);
-            controller=new Controller(this);
-            view=new View(this);
-        }
+		public RaceGame()
+		{
+			running = false;
+			map = new Map();
+			ui = new UI(this);
+			controller = new Controller(this);
+			view = new View(this);
+		}
 
-        public void render() {
-            map.render(view);
-            foreach (Entity entity in entities) {
-                entity.render(view);
-            }
-        }
+		public void render()
+		{
+			map.render(view);
+			foreach (Entity entity in entities)
+			{
+				entity.render(view);
+			}
+		}
 
-        public void update(int dt) {
-            Collision.CheckMapCollisions(dt, map, entities);
-            Collision.CheckEntityCollisions(dt, entities);
+		public void update(int dt)
+		{
+			Collision.CheckMapCollisions(dt, map, entities);
+			Collision.CheckEntityCollisions(dt, entities);
 
-            foreach (Entity entity in entities) {
-                if (!entity.update(dt, controller)) {
-                    entities.Remove(entity);
-                }
-            }
-        }
+			foreach (Entity entity in entities)
+			{
+				if (!entity.update(dt, controller))
+				{
+					entities.Remove(entity);
+				}
+			}
+		}
 
-        public void run()
-        {
-            nextTick = Environment.TickCount + 1000 / FPS;
-            int currentTick=Environment.TickCount;
-            int lastTick = currentTick;
-            running = true;
-            while (running) {
-                do {
-                    update(currentTick - lastTick);
-                    lastTick = currentTick;
-                } while ((currentTick = Environment.TickCount) < nextTick);
-                render();
-                nextTick += 1000 / FPS;
-            }
-        }
+		public void run()
+		{
+			nextTick = Environment.TickCount + 1000 / FPS;
+			int currentTick = Environment.TickCount;
+			int lastTick = currentTick;
+			running = true;
+			while (running)
+			{
+				do
+				{
+					update(currentTick - lastTick);
+					lastTick = currentTick;
+				} while ((currentTick = Environment.TickCount) < nextTick);
+				render();
+				nextTick += 1000 / FPS;
+			}
+		}
 
-        public Map Map {
-            get {
-                return map;
-            }
-            set {
-                map = value;
-            }
-        }
+		public Map Map
+		{
+			get
+			{
+				return map;
+			}
+			set
+			{
+				map = value;
+			}
+		}
 
-        public UI UI {
-            get {
-                return ui;
-            }
-            set {
-                ui = value;
-            }
-        }
-        public Controller Controller {
-            get {
-                return controller;
-            }
-            set {
-                controller = value;
-            }
-        }
-        public View View {
-            get {
-                return view;
-            }
-            set {
-                view = value;
-            }
-        }
-        public List<Entity> Entities {
-            get {
-                return entities;
-            }
-        }
-    }
+		public UI UI
+		{
+			get
+			{
+				return ui;
+			}
+			set
+			{
+				ui = value;
+			}
+		}
+		public Controller Controller
+		{
+			get
+			{
+				return controller;
+			}
+			set
+			{
+				controller = value;
+			}
+		}
+		public View View
+		{
+			get
+			{
+				return view;
+			}
+			set
+			{
+				view = value;
+			}
+		}
+		public List<Entity> Entities
+		{
+			get
+			{
+				return entities;
+			}
+		}
+	}
 }
