f12cfb7dfb57d288d216a1ae39a847aeb9cd31b5
gezichtshaar <tedteddet@gmail.com>
2013-10-15 15:34:06 +0200

    gedeelte van collision geschreven

--- XNARaceGame/Collision.cs
+++ XNARaceGame/Collision.cs
@@ -10,6 +10,18 @@ namespace XNARaceGame
         }
 
         public static void CheckEntityCollisions(int dt, List<Entity> entities) {
+            foreach (Entity entity in entities) {
+                if (entity.Actor) {
+                    foreach (Entity e in entities) {
+                        if (collides(entity, e)) {
+                            EntitiesCollide(entity, e);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void EntitiesCollide(Entity collider, Entity colliding) {
 
         }
 	}
