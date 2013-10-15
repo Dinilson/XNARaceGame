b5f2044ae16c7ca6c78bcf5f72228c7da63dc97c
erikjanhofman <erikjanhofman@gmail.com>
2013-10-15 15:23:40 +0200

    Constructor checkout updated

--- XNARaceGame/Checkpoint.cs
+++ XNARaceGame/Checkpoint.cs
@@ -2,13 +2,16 @@ using System;
 
 namespace XNARaceGame
 {
-	private class Checkpoint
+	private class Checkpoint : Entity
 	{
         int num;
 
-		public Checkpoint(int num)
+		public Checkpoint(int num, Vector2 coords, Vector2 hitbox, rot) : base("Checkpoint", coords, hitbox, false, true, rot)
 		{
             this.num = num;
+            //base.coords = coords;
+            //base.hitbox = hitbox;
+            //base.rot = rot;
 		}
 
         
