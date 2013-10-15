d38e80974fadcea4d39a3c3dd0dfc685ca0cfa0c
remynaps <remysije@gmail.com>
2013-10-15 15:02:48 +0200

    startet coding powerup(no idea what i'm doing)

--- XNARaceGame/Powerup.cs
+++ XNARaceGame/Powerup.cs
@@ -2,11 +2,61 @@ using System;
 
 namespace XNARaceGame
 {
-	public class Powerup
+	private class Powerup
 	{
+        bool isActive {get; set;} 
+        string type {get; set;} 
+        int timer {get; set;} 
+
 		public Powerup()
 		{
 		}
+
+        public Powerup(bool isA, string ty, int time)
+        {
+            isActive = isA;
+            type = ty;
+            timer = time;
+        }
+
+
+
+
+        public void resetDamage
+        {
+           Car.damage = 0.0;
+        } 
+        public void resetPetrol
+        {
+           Car.petrol = 100;
+        }
+        public void update
+        {
+
+
+        }
+        public void render
+        {
+
+        }
+
+        public void collision(Entity entity)
+        {
+
+            if (entity.Name() == "Car" && isActive)
+            {
+                if (type == "Damage")
+                {
+
+                    Powerup.resetDamage(entity);
+                }
+                else if (type == "Petrol")
+                {
+                    Powerup.resetPetrol(entity);
+                }
+                
+            }
+        }
 	}
 }
 
