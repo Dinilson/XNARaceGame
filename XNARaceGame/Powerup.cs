1115c7fae5edb3ec7255baf87dd6744e9f2426b0
Rubykuby <rubykuby@gmail.com>
2013-10-15 23:27:17 +0200

    Updated Powerup.

--- XNARaceGame/Powerup.cs
+++ XNARaceGame/Powerup.cs
@@ -1,53 +1,56 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace XNARaceGame
 {
-	class Powerup
+	class Powerup : Entity
 	{
-        bool isActive {get; set;} 
-        string type {get; set;} 
-        int timer {get; set;} 
+        public bool isActive {get; set;} 
+        public string type {get; set;} 
+        public int timer {get; set;}
 
-		public Powerup()
-		{
-		}
-
-        public Powerup(bool isActive, string type, int timer)
+        public Powerup(bool isActive, string type, int timer, double coords, Vector2 hitbox) : base("Powerup", coords, 0, hitbox, true, true)
         {
             this.isActive = isActive;
             this.type = type;
             this.timer = timer;
         }
 
-
-
-
-        public void resetDamage
+        public void resetDamage()
         {
            Car.damage = 0.0;
-        } 
-        public void resetPetrol
+        }
+
+        public void resetPetrol()
         {
            Car.petrol = 100;
         }
-        public void update
+
+        public void update()
         {
-
-
+			// Nothing is updated here.
         }
-        public void render
-        {
 
+        public void render()
+        {
+			// Nothing is rendered here.
         }
 
         public void collision(Entity entity)
         {
 
-            if (entity.Name() == "Car" && isActive)
+            if (entity.name == "Car" && isActive == true)
             {
                 if (type == "Repair")
                 {
-
                     Powerup.resetDamage(entity);
                 }
                 else if (type == "Petrol")
