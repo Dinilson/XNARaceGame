60bfbc7a29fe36b91a521bc7640ec47698204045
gezichtshaar <tedteddet@gmail.com>
2013-10-16 12:56:21 +0200

    begin gemakt met view class

--- XNARaceGame/View.cs
+++ XNARaceGame/View.cs
@@ -1,13 +1,21 @@
 using System;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
 
 namespace XNARaceGame
 {
 	public class View
 	{
+        public GraphicsDevice graphicsDevice { get; set; }
+        public SpriteBatch spriteBatch { get; set; }
+
         private RaceGame game;
 
 		public View(RaceGame game)
 		{
+            graphicsDevice = new GraphicsDevice();
+            spriteBatch = new SpriteBatch(graphicsDevice);
             this.game = game;
 		}
 	}
