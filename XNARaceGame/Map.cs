e81a168dbbf6a873de051af4539e98faaea05336
Rubykuby <rubykuby@gmail.com>
2013-10-16 10:39:16 +0200

    Fixed errors in Map.cs

--- XNARaceGame/Map.cs
+++ XNARaceGame/Map.cs
@@ -9,16 +9,9 @@ namespace XNARaceGame
 		{
 		}
 
-<<<<<<< HEAD
         public void render(View view)
         {
         }
-=======
-		public void render()
-		{
-			// Nothing is rendered here.
-		}
->>>>>>> 7f4774a5fc719d3e5363cea2cf9863904bacee7d
 	}
 }
 
