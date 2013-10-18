5c5a96730f2ded602cf15d72d01075eb66bc4949
Ruben Bakker <rubykuby@gmail.com>
2013-10-18 10:06:27 +0200

    Renamed class files

---
+++ XNARaceGame/InputManager.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace XNARaceGame
+{
+	public class InputManager
+	{
+        public List<char> keyMap { get; set; }
+
+        private RaceGame game;
+
+		public InputManager(RaceGame game)
+		{
+            this.game = game;
+            keyMap = new List<char>();
+		}
+
+        public void handleGameInput() {
+            if(keyMap.Contains('p')) {
+                game.paused = !game.paused;
+            }
+        }
+	}
+}
+
