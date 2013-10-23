b24b4e2b7ca0e74ec310ef5543bd0a08b13e673c
erikjanhofman <erikjanhofman@gmail.com>
2013-10-23 14:22:11 +0200

    checkpoint

--- XNARaceGame/XNARaceGame/Checkpoint.cs
+++ XNARaceGame/XNARaceGame/Checkpoint.cs
@@ -14,6 +14,7 @@ namespace XNARaceGame
 	class Checkpoint : Entity
 	{
         int num;
+        int startFinish, checkpoint1, checkpoint2, checkpoint3;
 
 		public Checkpoint(RaceGame game, int num, Vector2 coords, Vector2 hitbox, double rot) : base("Checkpoint", game, coords, hitbox, rot, false, true, false)
 		{
@@ -22,6 +23,19 @@ namespace XNARaceGame
 			// Because "Vector2 coords" is a parameter in "public Checkpoint()", it is forwarded to "coords" in base "base()".
 			// The same applies to "hitbox" and "rot".
 		}
+
+        public int CheckpointCount()
+        {
+            startFinish = 0;
+            checkpoint1 = startFinish + 1;
+            checkpoint2 = checkpoint1 + 1;
+            checkpoint3 = checkpoint2 + 1;
+
+            return startFinish;
+
+        }
+
+        public void 
 
         public override void render(GraphicsManager graphicsManager)
         {
@@ -33,10 +47,18 @@ namespace XNARaceGame
 
         public override void entityCollision(Entity entity)
         {
+            if (entity.name == "Car") 
+            {
+                setCheckpoint((Car)entity);
+            }
         }
 
         public override void mapCollision(Vector2 vector)
         {
+        }
+        private void setCheckpoint(Car car) {
+            car.checkpoints[num] = true;
+
         }
 	}
 }
