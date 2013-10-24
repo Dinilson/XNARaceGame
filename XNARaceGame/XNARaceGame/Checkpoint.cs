45d90ee3b4ebf248d089cd888ce9737669904e24
erikjanhofman <erikjanhofman@gmail.com>
2013-10-24 14:18:16 +0200

    checkpoint veranderingen

--- XNARaceGame/XNARaceGame/Checkpoint.cs
+++ XNARaceGame/XNARaceGame/Checkpoint.cs
@@ -14,37 +14,30 @@ namespace XNARaceGame
 	class Checkpoint : Entity
 	{
         int num;
-        int startFinish, checkpoint1, checkpoint2, checkpoint3;
 
-		public Checkpoint(RaceGame game, int num, Vector2 coords, Vector2 hitbox, double rot) : base("Checkpoint", game, coords, hitbox, rot, false, true, false)
+        #region constructor
+        public Checkpoint(RaceGame game, int num, Vector2 coords, Vector2 hitbox, double rot) : base("Checkpoint", game, coords, hitbox, rot, false, true, false)
 		{
             this.game = game;
             this.num = num;
 			// Because "Vector2 coords" is a parameter in "public Checkpoint()", it is forwarded to "coords" in base "base()".
 			// The same applies to "hitbox" and "rot".
 		}
+        #endregion
 
-        public int CheckpointCount()
-        {
-            startFinish = 0;
-            checkpoint1 = startFinish + 1;
-            checkpoint2 = checkpoint1 + 1;
-            checkpoint3 = checkpoint2 + 1;
-
-            return startFinish;
-
-        }
-
-        
-
+        #region render
         public override void render(GraphicsManager graphicsManager)
         {
         }
+        #endregion
 
+        #region update
         public override bool update(float dt, InputManager inputManager) {
             return isAlive;
         }
+        #endregion
 
+        #region entityCollision
         public override void entityCollision(Entity entity)
         {
             if (entity.name == "Car") 
@@ -52,12 +45,37 @@ namespace XNARaceGame
                 setCheckpoint((Car)entity);
             }
         }
+        #endregion
 
+        #region mapCollision
         public override void mapCollision(Vector2 vector)
         {
         }
-        private void setCheckpoint(Car car) {
-            car.checkpoints[num] = true;
+        #endregion
+
+        #region setCheckpoint
+        private void setCheckpoint(Car car) 
+        {
+            if (num > 0 && car.checkpoints[num - 1])
+            {
+                car.checkpoints[num] = true;
+            }
+            else if (num == car.checkpoints.Length - 1 && car.checkpoints[num - 1])
+            {
+                //ronde is klaar
+            }
+
+            else if (num == 0)
+            {
+                car.checkpoints[num] = true;
+            }
+        #endregion
+
+
+
+
+
+
 
         }
 	}
