5423544d0982a837dbfcf420a9db210c10679c62
gezichtshaar <tedteddet@gmail.com>
2013-10-30 17:03:16 +0100

    small change

--- XNARaceGame/XNARaceGame/GraphicsManager.cs
+++ XNARaceGame/XNARaceGame/GraphicsManager.cs
@@ -63,7 +63,8 @@ namespace XNARaceGame {
         public void setViewportCoords (Vector2 coords) {
             //int screenX = Math.Max(SCREEN_WIDTH / 2, Math.Min(1920 - (int)(SCREEN_WIDTH/2), (int)coords.X -SCREEN_WIDTH/2));
             //int screenY = Math.Max(SCREEN_HEIGHT / 2, Math.Min(1080 - (int)(SCREEN_HEIGHT/2) / 2, (int)coords.Y - SCREEN_HEIGHT / 2));
-            nextViewportCoords = Vector2.Subtract(coords * SCALE, new Vector2((coords.X * SCALE - SCREEN_WIDTH / 2) > 0 ? SCREEN_WIDTH / 2: 0, SCREEN_HEIGHT / 2));
+            nextViewportCoords = Vector2.Subtract(coords * SCALE, new Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2));
+            nextViewportCoords = Vector2.Clamp(nextViewportCoords, new Vector2(0, 0), new Vector2(1080, 1920));
         }
 
         public void updateViewport() {
