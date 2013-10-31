d94cad103655f362546b01b2b80af4d4607aa149
erikjanhofman <erikjanhofman@gmail.com>
2013-10-31 14:06:34 +0100

    Spritefont toegevoegd en vastgelopen=(
    =D=D

--- XNARaceGame/XNARaceGame/UI.cs
+++ XNARaceGame/XNARaceGame/UI.cs
@@ -1,10 +1,23 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace XNARaceGame
 {
     // tekent de userinterface
 	public class UI
 	{
+        SpriteFont Font1;
+        Vector2 FontPos;
+
         private RaceGame game;
 
 		public UI(RaceGame game)
@@ -12,8 +25,12 @@ namespace XNARaceGame
             this.game = game;
 		}
 
-        public void render(GraphicsManager graphicsManager) {
+        public void render(GraphicsManager graphicsManager) 
+        {
+            //graphicsManager.spriteBatch.DrawString(Font1,"........................
+
             
+
         }
 	}
 }
