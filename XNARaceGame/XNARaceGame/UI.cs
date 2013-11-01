9edc656de4fbd40d77ac5812c02257683a8bf8bc
gezichtshaar <tedteddet@gmail.com>
2013-11-01 09:52:44 +0100

    typo's gefixt

--- XNARaceGame/XNARaceGame/UI.cs
+++ XNARaceGame/XNARaceGame/UI.cs
@@ -24,7 +24,7 @@ namespace XNARaceGame
 
         public void render(GraphicsManager graphicsManager) 
         {
-            graphicsManager.drawText("font1", "dit is een text", new Vector(10, 10), Color.white, true);
+            graphicsManager.drawText("font1", "dit is een text", new Vector2(10, 10), Color.White, true);
         }
 	}
 }
